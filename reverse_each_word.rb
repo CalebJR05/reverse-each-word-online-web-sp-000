bdc513cf6bbdb120c21dd605b8d5346d06acde01
Caleb R <cjr4912@gmail.com>
2020-07-27 15:35:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,6 @@
-def reverse_each_word(sentence1)
-  sentence1.split
-  sentence1.each do |word|
-      puts word.reverse
-      
-  end 
-end
-
-def reverse_each_word(sentence2)
-  sentence2.split
-  sentence2.collect do |word|
+def reverse_each_word(sentence)
+  sentence.split
+  sentence.collect do |word|
       puts word.reverse
       
   end
