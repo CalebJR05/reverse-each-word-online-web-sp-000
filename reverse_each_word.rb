b5c1c50cd44bebb7766635a42c0a68bc5e477333
Caleb R <cjr4912@gmail.com>
2020-07-27 15:31:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,15 @@
 def reverse_each_word(words)
   words.split
-    words.each do |word|
+  words.each do |word|
       puts word.reverse
-    end 
-  end
-end 
+      
+  end 
+end
 
 def reverse_each_word(words)
   words.split
-    words.collect do |word|
+  words.collect do |word|
       puts word.reverse
-    end
+      
   end
 end
